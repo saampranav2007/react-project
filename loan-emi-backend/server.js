16192442ad72cd285abc83741b2229616cb324c3
SAAMPRANAV T <saampranav27@gmail.com>
2026-04-10 14:33:29 +0530

    Add new CORS origin for Vercel deployment

--- loan-emi-backend/server.js
+++ loan-emi-backend/server.js
@@ -14,12 +14,11 @@ const app = express();
 app.use(cors({
     origin: [
         "http://localhost:5173", 
-        "https://react-project-kf6u.onrender.com" 
+        "https://loanemi.vercel.app" // 
     ],
     credentials: true, 
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
-// ==========================================
 
 app.use(express.json());
 
